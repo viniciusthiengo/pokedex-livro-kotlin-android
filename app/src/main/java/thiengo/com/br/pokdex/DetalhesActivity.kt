a00b710ce6464e5df45b4c4107813f6502df0254
Vinícius Thiengo <viniciusthiengo@gmail.com>
2018-06-09 19:22:00 -0300

    Initial commit

---
+++ app/src/main/java/thiengo/com/br/pokdex/DetalhesActivity.kt
@@ -0,0 +1,80 @@
+package thiengo.com.br.pokdex
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_detalhes.*
+import kotlinx.android.synthetic.main.content_detalhes.*
+import thiengo.com.br.pokdex.domain.Pokemon
+
+
+class DetalhesActivity :
+        AppCompatActivity() {
+
+    lateinit var pokemon : Pokemon
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+
+        setSupportActionBar(toolbar)
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true)
+
+        fab.setOnClickListener {
+            customToast()
+        }
+
+        ct_layout.setCollapsedTitleTypeface( ResourcesCompat.getFont(this, R.font.pokemon_solid) )
+        ct_layout.setExpandedTitleTypeface( ResourcesCompat.getFont(this, R.font.pokemon_solid) )
+
+        pokemon = intent.getParcelableExtra<Pokemon>(Pokemon.KEY)
+
+        iv_header.setImageResource( pokemon.imagemRes ?: 0 )
+        tv_descricao.text = pokemon.descricao
+        tv_numero.text = pokemon.numero.toString().padStart(3, '0')
+        tv_altura.text = pokemon.altura
+        tv_peso.text = pokemon.peso
+        tv_sexo.text = pokemon.sexo
+        tv_categoria.text = pokemon.categoria
+        tv_habilidades.text = pokemon.getHabilidadesFormatado()
+        tv_tipo.text = pokemon.getTipoFormatado()
+        tv_fraquezas.text = pokemon.getFraquezasFormatado()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toolbar.title = pokemon.nome
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun customToast(){
+        val llToast = layoutInflater.inflate(
+                R.layout.toast_custom,
+                findViewById(R.id.ll_toast))
+
+        val tvInfo = llToast.findViewById<TextView>( R.id.tv_info )
+
+        tvInfo.typeface = Typeface.create(
+                "sans-serif",
+                Typeface.NORMAL)
+
+        val toast = Toast( applicationContext )
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = llToast
+        toast.show()
+    }
+}
---
+++ app/src/main/java/thiengo/com/br/pokdex/PokemonsActivity.kt
@@ -0,0 +1,112 @@
+package thiengo.com.br.pokdex
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.text.SpannableString
+import android.text.Spanned
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_pokemons.*
+import kotlinx.android.synthetic.main.app_bar_pokemons.*
+import thiengo.com.br.pokdex.data.Mock
+import thiengo.com.br.pokdex.util.CustomTypefaceSpan
+
+
+class PokemonsActivity :
+        AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemons)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            customSnackbar( it )
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        customFontNavigationViewItem()
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        rv_pokemons.setHasFixedSize(true)
+
+        val mLayoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        rv_pokemons.layoutManager = mLayoutManager
+
+        rv_pokemons.adapter = PokemonsAdapter(this, Mock.getPokemons())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.pokemons, menu)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
+    fun customSnackbar( view: View){
+        val snackbar = Snackbar
+                .make( view,"Ainda falta implementar uma ação", Snackbar.LENGTH_LONG)
+                .setAction("Ação", null)
+
+        val tv = snackbar.view.findViewById<TextView>( android.support.design.R.id.snackbar_text )
+        tv.typeface = Typeface.create("sans-serif", Typeface.NORMAL)
+
+        snackbar.show()
+    }
+
+    fun customFontNavigationViewItem(){
+        val sansSerif = Typeface.create("sans-serif", Typeface.NORMAL)
+        val pokemonSolid = ResourcesCompat.getFont(this, R.font.pokemon_solid) ?: sansSerif
+        val menu = nav_view.menu
+
+        for( i in 0..menu.size() - 1 ){
+            val item = menu.getItem( i )
+            setCustomFontItem( item, sansSerif )
+
+            if( item.subMenu != null ){
+                setCustomFontItem( item, pokemonSolid )
+                val subMenu = item.subMenu
+
+                for( j in 0..subMenu.size() - 1 ) {
+                    val subItem = subMenu.getItem(j)
+                    setCustomFontItem( subItem, sansSerif )
+                }
+            }
+        }
+    }
+
+    fun setCustomFontItem( item: MenuItem, typeface: Typeface ){
+        val textItem = SpannableString( item.title )
+        textItem.setSpan(
+                CustomTypefaceSpan( typeface ),
+                0,
+                textItem.length,
+                Spanned.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        item.title = textItem
+    }
+}
+
+
